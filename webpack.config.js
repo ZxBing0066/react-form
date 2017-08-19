6cd48d60b91756f79c5b00a9f9d932e8daa60c27
ZxBing0066 <ZxBing0066@gmail.com>
2017-08-19 18:13:32 +0800

    add dev config

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,24 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: path.resolve(__dirname, 'index.jsx'),
-    devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
         hot: true
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
+    plugins: [new webpack.HotModuleReplacementPlugin()],
     module: {
-        loaders: [{
-            test: /\.js(x)?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-        }]
+        loaders: [
+            {
+                test: /\.js(x)?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
+    resolve: {
+        extensions: ['.js', '.json', '.jsx', '.css']
+    },
+    externals: ['react', 'prop-types'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
---
+++ webpack.dev.js
@@ -0,0 +1,5 @@
+let webpackConfig = require('./webpack.config');
+
+webpackConfig.devtool = 'source-map';
+
+module.exports = webpackConfig;
